c3658ae9e2bf5bbc0a29dd61abc14662e85b819f
Kaspar Thommen <kaspar.thommen@gmail.com>
2023-04-11 11:09:06 +0200

    NLP WIP

--- src/python/nice_lowpass.py
+++ src/python/nice_lowpass.py
@@ -3,25 +3,26 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 import numpy as np
 
-from src.python.zero_pole import ZeroPole
+from src.python.zero_pole import ZeroPole, ZeroPoleType
 
 
 # impulse responses
+type = ZeroPoleType.ANALOG
 T = 60
 tau = 20
 t = np.arange(T+1)
 
 
 def nice_lowpass(order: int, tau: float) -> ZeroPole:
-    nlp = ZeroPole.ZERO
+    nlp = ZeroPole.zero(type)
     for i in range(order):
-        nlp += ZeroPole.lp1(w_pole=order/tau)**(i+1)
+        nlp += ZeroPole.lp1(type=type, tau=tau/order)**(i+1)
 
     nlp /= order
     return nlp
 
 
-lp1 = ZeroPole.lp1(w_pole=1/tau)
+lp1 = ZeroPole.lp1(type=type, tau=tau)
 nlp4 = nice_lowpass(order=4, tau=tau)
 
 y_sma = np.zeros_like(lp1.impulse(T+1))
@@ -29,7 +30,7 @@ y_sma[(t >= 0) & (t < tau)] = 1/tau
 
 
 # plot & save
-def finalize_plot(tau: float, file_name: str, legend: bool=True, y_lim: float=np.nan) -> None:
+def finalize_plot(tau: float, file_name: str, legend: bool = True, y_lim: float = np.nan) -> None:
     ax.set_xticks(np.linspace(0, T, 7))
     ax.set_xlim(0, T)
     ax.set_ylim(0, 1.05/tau if np.isnan(y_lim) else y_lim)
@@ -52,7 +53,7 @@ fig, ax = plt.subplots(figsize=figsize)
 fig.suptitle('Simple and exponential moving averages')
 ax.plot(t, y_sma, marker=marker, drawstyle=drawstyle, lw=lw, label=f'FIR')
 ax.plot(t, lp1.impulse(T+1), marker=marker, drawstyle=drawstyle, lw=lw, label=f'LP-1')
-finalize_plot(tau, "sma+ema.png")
+finalize_plot(tau=tau, file_name="sma+ema.png")
 
 # # SMA, LP-1 & NLP
 fig, ax = plt.subplots(figsize=figsize)
@@ -60,7 +61,7 @@ fig.suptitle('Adding a "nice low-pass" to the mix')
 ax.plot(t, y_sma, marker=marker, drawstyle=drawstyle, lw=lw, label=f'FIR')
 ax.plot(t, lp1.impulse(T+1), marker=marker, drawstyle=drawstyle, lw=lw, label=f'LP-1')
 ax.plot(t, nlp4.impulse(T+1), marker=marker, drawstyle=drawstyle, lw=lw, label=f'Nice Low-Pass')
-finalize_plot(tau, "sma+ema+nlp4.png")
+finalize_plot(tau=tau, file_name="sma+ema+nlp4.png")
 
 # # NLPs
 # fig, ax = plt.subplots(figsize=figsize)
@@ -72,20 +73,47 @@ finalize_plot(tau, "sma+ema+nlp4.png")
 #     if order == 1:
 #         label += f' = LP-1'
 #     ax.plot(t, nlp.impulse(T+1), marker=marker, drawstyle=drawstyle, lw=lw, label=label)
-# finalize_plot(tau, "sma+nlps.png")
+# finalize_plot(tau=tau, file_name="sma+nlps.png")
 
 # bump
 fig, ax = plt.subplots(figsize=figsize)
 fig.suptitle('A "bump" function')
-lp4 = ZeroPole.lp1(w_pole=4/tau)**4
+lp4 = ZeroPole.lp1(type=type, tau=tau/4)**4
 ax.plot(t, lp4.impulse(T + 1), marker=marker, drawstyle=drawstyle, lw=lw)
-finalize_plot(tau, "bump.png", legend=False)
+finalize_plot(tau=tau, file_name="bump.png", legend=False)
 
 # LP-n
 fig, ax = plt.subplots(figsize=figsize)
 fig.suptitle('LP-n')
 for order in [1, 2, 4, 8]:
-    nlp = ZeroPole.lp1(w_pole=4/tau)**order
+    lpn = ZeroPole.lp1(type=type, tau=tau/4)**order
     label = f'LP-{order}'
-    ax.plot(t, nlp.impulse(T+1), marker=marker, drawstyle=drawstyle, lw=lw, label=label)
-finalize_plot(tau, "lps.png", y_lim=4.2/tau)
+    ax.plot(t, lpn.impulse(T+1), marker=marker, drawstyle=drawstyle, lw=lw, label=label)
+finalize_plot(tau=tau, file_name="lps.png", y_lim=4.2/tau)
+
+
+w = np.linspace(0, 0.5, 1000)
+fig, [ax1, ax2] = plt.subplots(figsize=[figsize[0], 2*figsize[1]], nrows=2)
+fig.suptitle('Bode plot')
+ax1.set_title('Magnitude response')
+ax2.set_title('Phase response [deg]')
+# min_mag_db = 0
+for order in [1, 2, 4, 8]:
+    nlp = nice_lowpass(order=order, tau=100)
+    label = f'NLP-{order}' if order >= 2 else 'LP-1'
+    mag = nlp.magnitude(w)
+    # mag_db = 10*np.log10(mag)
+    # mag_db = 3*np.log2(mag)
+    # min_mag_db = min(min_mag_db, np.min(mag_db))
+    # ax1.semilogx(w, mag_db, label=label)
+    ax1.loglog(w, mag, label=label)
+    ax2.semilogx(w, nlp.phase(w)*180/np.pi)
+ax1.spines[['right', 'top']].set_visible(False)
+ax2.spines[['right', 'top']].set_visible(False)
+ax1.xaxis.set_zorder(99)
+ax2.xaxis.set_zorder(99)
+# ax1.set_yticks(np.arange(0, min_mag_db, -3))
+ax1.legend()
+fig.tight_layout()
+plt.savefig(Path('posts/nice-lowpass') / 'bode.png')
+plt.show()
--- src/python/zero_pole.py
+++ src/python/zero_pole.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 
+from enum import Enum
 from typing import Union, Any, List
 import numpy as np
-from scipy.signal import dimpulse
-
+from scipy.signal import dimpulse, impulse
 
 ARRAY = Union[List[float], np.ndarray]
 COMPLEX_ARRAY = Union[List[float], List[complex], np.ndarray]
@@ -15,6 +15,7 @@ def _poly(a: COMPLEX_ARRAY) -> np.ndarray:
     if np.all(np.isreal(a)):
         poly = np.poly(a)
     else:
+        # combine complex conjugate pairs if present
         a = np.sort(a)
         poly = [1]
         while len(a) >= 2:
@@ -35,7 +36,7 @@ def _poly(a: COMPLEX_ARRAY) -> np.ndarray:
     return np.atleast_1d(poly)
 
 
-def matching_entries(
+def _matching_entries(
     a: np.ndarray,
     b: np.ndarray,
     remove: bool = False,
@@ -59,8 +60,26 @@ def matching_entries(
     return a, b if remove else np.array(entries)
 
 
+class ZeroPoleType(Enum):
+    ANALOG = "analog"
+    DIGITAL = "digital"
+
+
 class ZeroPole:
     """
+    If analog (no delay allowed, but pure differentiators):
+
+                   ┬┬  ╭                 ╮
+                   ││  │1 - zeros[i] * s │
+                       ╰                 ╯    diffs
+    H(s) = gain * ──────────────────────── * s
+                   ┬┬  ╭                 ╮
+                   ││  │1 - poles[j] * s │
+                       ╰                 ╯
+
+
+    If digital (delay allowed, but)
+
                    ┬┬  ╭                -1 ╮
                    ││  │1 - zeros[i] * z   │
                        ╰                   ╯    -delay
@@ -70,13 +89,58 @@ class ZeroPole:
                        ╰                   ╯
     """
 
-    ZERO: ZeroPole
-    ONE: ZeroPole
-    DIFF: ZeroPole
-    DELAY: ZeroPole
+    ZERO_ANALOG: ZeroPole
+    ZERO_DIGITAL: ZeroPole
 
-    def __init__(self, gain: float, zeros: ARRAY, poles: ARRAY, delay: int = 0) -> None:
-        assert delay >= 0
+    ONE_ANALOG: ZeroPole
+    ONE_DIGITAL: ZeroPole
+
+    DIFF_ANALOG: ZeroPole
+    DIFF_DIGITAL: ZeroPole
+
+    DELAY_DIGITAL: ZeroPole
+
+    @classmethod
+    def analog(
+            cls,
+            gain: float,
+            zeros: ARRAY,
+            poles: ARRAY,
+            diffs: int = 0
+    ) -> ZeroPole:
+        return ZeroPole(
+            type=ZeroPoleType.ANALOG,
+            gain=gain,
+            zeros=zeros,
+            poles=poles,
+            delay_or_diffs=diffs)
+
+    @classmethod
+    def digital(
+            cls,
+            gain: float,
+            zeros: ARRAY,
+            poles: ARRAY,
+            delay: int = 0
+    ) -> ZeroPole:
+        return ZeroPole(
+            type=ZeroPoleType.DIGITAL,
+            gain=gain,
+            zeros=zeros,
+            poles=poles,
+            delay_or_diffs=delay)
+
+    def __init__(
+            self,
+            type: ZeroPoleType,
+            gain: float,
+            zeros: ARRAY,
+            poles: ARRAY,
+            delay_or_diffs: int = 0
+    ) -> None:
+        assert type in ZeroPoleType
+        if type == ZeroPoleType.DIGITAL:
+            assert delay_or_diffs >= 0  # remain causal
 
         zeros = np.sort(zeros)
         poles = np.sort(poles)
@@ -84,15 +148,16 @@ class ZeroPole:
         # assert not np.any(np.iscomplex(zeros))
         # assert not np.any(np.iscomplex(poles))
 
-        zeros, poles = matching_entries(zeros, poles, remove=True)
+        zeros, poles = _matching_entries(zeros, poles, remove=True)
 
+        self.type = type
         self.gain = gain
         self.poles = np.real_if_close(poles, tol=1_000_000)
         self.zeros = np.real_if_close(zeros, tol=1_000_000)
-        self.delay = delay
+        self.delay_or_diffs = delay_or_diffs
 
     def __neg__(self) -> ZeroPole:
-        return ZeroPole(-self.gain, self.zeros, self.poles, self.delay)
+        return ZeroPole(self.type, -self.gain, self.zeros, self.poles, self.delay_or_diffs)
 
     def __radd__(self, other: Any) -> ZeroPole:
         return self + other
@@ -105,10 +170,12 @@ class ZeroPole:
 
     def __add__(self, other: Any) -> ZeroPole:
         if isinstance(other, float) or isinstance(other, int):
-            return self + ZeroPole(other, [], [])
+            return self + ZeroPole(self.type, other, [], [])
         elif isinstance(other, ZeroPole):
+            self._ensure_same_type(other)
+
             # compute new poles
-            poles_to_add_left, poles_to_add_right = matching_entries(
+            poles_to_add_left, poles_to_add_right = _matching_entries(
                 other.poles,
                 self.poles,
                 remove=True
@@ -122,36 +189,38 @@ class ZeroPole:
             zeros_right = np.concatenate([other.zeros, poles_to_add_right])
             num_left = self.gain * _poly(zeros_left)
             num_right = other.gain * _poly(zeros_right)
-            num_left = np.pad(num_left, (self.delay, 0))
-            num_right = np.pad(num_right, (other.delay, 0))
+            num_left = np.pad(num_left, (self.delay_or_diffs, 0))
+            num_right = np.pad(num_right, (other.delay_or_diffs, 0))
             left_minus_right = len(num_left) - len(num_right)
             num_left = np.pad(num_left, (0, max(0, -left_minus_right)))
             num_right = np.pad(num_right, (0, max(0, left_minus_right)))
             poly = num_left + num_right
             poly = np.trim_zeros(poly, trim='b')
             if len(poly) == 0:
-                return ZeroPole.ZERO
+                return ZeroPole.ZERO_ANALOG if self.type == ZeroPoleType.ANALOG else ZeroPole.ZERO_DIGITAL
 
             poly_clean = np.trim_zeros(poly, trim='f')
             delay = len(poly) - len(poly_clean)
             zeros = np.roots(poly_clean)  # may become imaginary, which is ok
             gain = poly_clean[0] / _poly(zeros)[0]
             if gain == 0:
-                return ZeroPole.ZERO
+                return ZeroPole.ZERO_ANALOG if self.type == ZeroPoleType.ANALOG else ZeroPole.ZERO_DIGITAL
 
-            return ZeroPole(gain, zeros, poles, delay)
+            return ZeroPole(self.type, gain, zeros, poles, delay)
         else:
             raise Exception()
 
     def __mul__(self, other: Any) -> ZeroPole:
         if isinstance(other, float) or isinstance(other, int):
-            return ZeroPole(self.gain * other, self.zeros, self.poles, self.delay)
+            return ZeroPole(self.type, self.gain * other, self.zeros, self.poles, self.delay_or_diffs)
         elif isinstance(other, ZeroPole):
+            self._ensure_same_type(other)
             return ZeroPole(
+                type=self.type,
                 gain=self.gain * other.gain,
                 zeros=np.concatenate([self.zeros, other.zeros]),
                 poles=np.concatenate([self.poles, other.poles]),
-                delay=self.delay + other.delay
+                delay_or_diffs=self.delay_or_diffs + other.delay_or_diffs
             )
         else:
             raise Exception()
@@ -161,25 +230,35 @@ class ZeroPole:
 
     def __truediv__(self, other: Any) -> ZeroPole:
         if isinstance(other, (float, int)):
-            return ZeroPole(self.gain / other, self.zeros, self.poles, self.delay)
+            return ZeroPole(
+                type=self.type,
+                gain=self.gain / other,
+                zeros=self.zeros,
+                poles=self.poles,
+                delay_or_diffs=self.delay_or_diffs
+            )
         elif isinstance(other, ZeroPole):
-            delay = self.delay - other.delay
+            self._ensure_same_type(other)
+            delay = self.delay_or_diffs - other.delay_or_diffs
             if delay < 0:
                 raise Exception('Result of division would be non-causal')
             return ZeroPole(
+                type=self.type,
                 gain=self.gain / other.gain,
                 zeros=np.concatenate([self.zeros, other.poles]),
                 poles=np.concatenate([self.poles, other.zeros]),
-                delay=delay
+                delay_or_diffs=delay
             )
         else:
             raise TypeError(f"Unknown type: {type(other)}")
 
     def __rtruediv__(self, other: Any) -> ZeroPole:
         if isinstance(other, float) or isinstance(other, int):
-            return ZeroPole(other / self.gain, self.poles, self.zeros, self.delay)
+            return ZeroPole(self.type, other / self.gain, self.poles, self.zeros, self.delay_or_diffs)
         # elif isinstance(other, ZeroPole):
+        #     self._ensure_same_type(other)
         #     return ZeroPole(
+        #         type=self.type,
         #         gain=self.gain / other.gain,
         #         zeros=np.concatenate([self.zeros, other.poles]),
         #         poles=np.concatenate([self.poles, other.zeros]))
@@ -194,7 +273,7 @@ class ZeroPole:
             raise Exception('power must be >= 0')
 
         if power == 0:
-            return ZeroPole.ONE
+            return ZeroPole.ONE_ANALOG if self.type == ZeroPoleType.ANALOG else ZeroPole.ONE_DIGITAL
 
         zp = self
         result = self
@@ -210,12 +289,20 @@ class ZeroPole:
         return self / self.static_gain()
 
     def impulse(self, n: int) -> np.ndarray:
-        sys = (*self.num_den(), 1)
-        t, y = dimpulse(system=sys, n=n)
-        return y[0].squeeze()
+        if self.type == ZeroPoleType.ANALOG:
+            num, den = self.num_den()
+            sys = (num[::-1], den[::-1])  # num, den (reverse order)
+            t, y = impulse(system=sys, T=np.arange(n))
+            return y
+        elif self.type == ZeroPoleType.DIGITAL:
+            sys = (*self.num_den(), 1)  # num, den, dt
+            t, y = dimpulse(system=sys, t=np.arange(n))
+            return y[0].squeeze()
+        else:
+            raise Exception
 
     def num_den(self) -> tuple[np.ndarray, np.ndarray]:
-        num = self.gain * np.pad(_poly(self.zeros), (self.delay, 0))
+        num = self.gain * np.pad(_poly(self.zeros), (self.delay_or_diffs, 0))
         den = _poly(self.poles)
         num_minus_den = len(num) - len(den)
         num = np.pad(num, (0, max(0, -num_minus_den)))
@@ -224,40 +311,109 @@ class ZeroPole:
 
     def magnitude(self, w: np.ndarray) -> np.ndarray:
         num, den = self.num_den()
-        z = np.exp(w * 1j)
-        n = np.polyval(num, z)
-        d = np.polyval(den, z)
-        return np.abs(n / d)
+        s_or_z = self._compute_s_or_z(w)
+        n = np.polyval(num[::-1], s_or_z)
+        d = np.polyval(den[::-1], s_or_z)
+        response = n / d
+        return np.abs(response)
 
-    @staticmethod
-    def lp1(w_pole: float) -> ZeroPole:
-        return ZeroPole(w_pole / (1 + w_pole), [], [1 / (1 + w_pole)])
+    def phase(self, w: np.ndarray) -> np.ndarray:
+        num, den = self.num_den()
+        s_or_z = self._compute_s_or_z(w)
+        n = np.polyval(num[::-1], s_or_z)
+        d = np.polyval(den[::-1], s_or_z)
+        response = n / d
+        return np.arctan2(response.imag, response.real)
 
-    @staticmethod
-    def hp1(w_zero: float) -> ZeroPole:
-        return ZeroPole(1 / (1 + w_zero), [1], [1 / (1 + w_zero)])
+    def _compute_s_or_z(self, w: np.ndarray) -> np.ndarray:
+        if self.type == ZeroPoleType.ANALOG:
+            # s -> j * w
+            return 1j * w
+        elif self.type == ZeroPoleType.DIGITAL:
+            # z^-1 -> exp(-j * w)
+            return np.exp(-1j * w)
+        else:
+            raise Exception
+
+    @classmethod
+    def zero(cls, type: ZeroPoleType) -> ZeroPole:
+        return ZeroPole.ZERO_ANALOG if type == ZeroPoleType.ANALOG else ZeroPole.ZERO_DIGITAL
+
+    @classmethod
+    def one(cls, type: ZeroPoleType) -> ZeroPole:
+        return ZeroPole.ONE_ANALOG if type == ZeroPoleType.ANALOG else ZeroPole.ONE_DIGITAL
+
+    @classmethod
+    def lp1(cls, type: ZeroPoleType, tau: float) -> ZeroPole:
+        assert type in ZeroPoleType
+        if type == ZeroPoleType.ANALOG:
+            # H(s) = 1 / (tau*s + 1) = 1 / (1 - (-tau)*s)
+            return ZeroPole.analog(
+                gain=1,
+                zeros=[],
+                poles=[-tau],
+            )
+        elif type == ZeroPoleType.DIGITAL:
+            # H(z1) = h / (tau+h - tau*z1)
+            return ZeroPole.digital(
+                gain=1 / (tau + 1),
+                zeros=[],
+                poles=[tau / (tau + 1)]
+            )
+        else:
+            raise Exception
+
+    @classmethod
+    def hp1(cls, type: ZeroPoleType, tau: float) -> ZeroPole:
+        assert type in ZeroPoleType
+        if type == ZeroPoleType.ANALOG:
+            # H(s) = (tau * s) / (tau * s + 1) = tau * s / (1 - (-tau)*s)
+            return ZeroPole.analog(
+                gain=tau,
+                zeros=[],
+                poles=[-tau],
+                diffs=1
+            )
+        elif type == ZeroPoleType.DIGITAL:
+            return ZeroPole.digital(
+                gain=tau / (1 + tau),
+                zeros=[1],
+                poles=[tau / (1 + tau)]
+            )
+        else:
+            raise Exception
 
     def order(self) -> int:
         return max(len(self.zeros), len(self.poles))
 
+    def _ensure_same_type(self, other):
+        if self.type != other.type:
+            raise TypeError('Cannot add analog and digital transfer functions')
+
     def __eq__(self, other: Any) -> bool:
         if isinstance(other, float) or isinstance(other, int):
-            other = ZeroPole(other, [], [])
+            other = ZeroPole(self.type, other, [], [])
 
         if isinstance(other, ZeroPole):
-            return np.isclose(self.gain, other.gain, rtol=RTOL, atol=ATOL) \
-                   and np.allclose(self.zeros, other.zeros, rtol=RTOL, atol=ATOL) \
-                   and np.allclose(self.poles, other.poles, rtol=RTOL, atol=ATOL) \
-                   and self.delay == other.delay
+            return self.type == other.type \
+                and np.isclose(self.gain, other.gain, rtol=RTOL, atol=ATOL) \
+                and self._allclose(self.zeros, other.zeros, rtol=RTOL, atol=ATOL) \
+                and self._allclose(self.poles, other.poles, rtol=RTOL, atol=ATOL) \
+                and self.delay_or_diffs == other.delay_or_diffs
 
         return False
 
     def __repr__(self) -> str:
-        return f'{ZeroPole.__name__}(gain={self.gain}, zeros={self.zeros}, poles={self.poles}, delay={self.delay})'
+        if self.type == ZeroPoleType.ANALOG:
+            return f'{ZeroPole.__name__}-{self.type}(gain={self.gain}, zeros={self.zeros}, poles={self.poles}, diffs={self.delay_or_diffs})'
+        elif self.type == ZeroPoleType.DIGITAL:
+            return f'{ZeroPole.__name__}-{self.type}(gain={self.gain}, zeros={self.zeros}, poles={self.poles}, delay={self.delay_or_diffs})'
+        else:
+            raise Exception
 
     def __str__(self) -> str:
-        num1, num2, num3 = self._compute_side(self.zeros)
-        den1, den2, den3 = self._compute_side(self.poles)
+        num1, num2, num3 = self._compute_side(self.type, self.zeros)
+        den1, den2, den3 = self._compute_side(self.type, self.poles)
 
         gain = f"{self.gain} * "
         w = max(len(num1), len(den1))
@@ -268,36 +424,57 @@ class ZeroPole:
         num1, num2, num3 = num1.rjust(ng), num2.rjust(ng), num3.rjust(ng)
         den1, den2, den3 = den1.rjust(ng), den2.rjust(ng), den3.rjust(ng)
         mid = f"{gain}{w * '─'}"
-        if self.delay != 0:
+        if self.delay_or_diffs != 0:
             mid += f" * z"
-            num3 += f"    -{self.delay}"
+            num3 += f"    -{self.delay_or_diffs}"
 
         return "\n".join([num1, num2, num3, mid, den1, den2, den3])
 
     @staticmethod
-    def _compute_side(x: np.ndarray) -> tuple[str, str, str]:
+    def _compute_side(type: ZeroPoleType, x: np.ndarray) -> tuple[str, str, str]:
         if len(x) == 0:
             return " ", "1", ""
 
-        row2 = [f"│1 - {xi} * z   │" for xi in x]
-        row1 = "   ".join([f"╭{(len(r) - 5) * ' '}-1 ╮" for r in row2])
-        row3 = "   ".join([f"╰{(len(r) - 2) * ' '}╯" for r in row2])
-        row2 = " * ".join(row2)
-        assert len(row1) == len(row2)
-        assert len(row2) == len(row3)
-        return row1, row2, row3
+        if type == ZeroPoleType.ANALOG:
+            row2 = [f"│1 - {xi} * s │" for xi in x]
+            row1 = "   ".join([f"╭{(len(r) - 3) * ' '} ╮" for r in row2])
+            row3 = "   ".join([f"╰{(len(r) - 2) * ' '}╯" for r in row2])
+            row2 = " * ".join(row2)
+            assert len(row1) == len(row2)
+            assert len(row2) == len(row3)
+            return row1, row2, row3
+        elif type == ZeroPoleType.DIGITAL:
+            row2 = [f"│1 - {xi} * z   │" for xi in x]
+            row1 = "   ".join([f"╭{(len(r) - 5) * ' '}-1 ╮" for r in row2])
+            row3 = "   ".join([f"╰{(len(r) - 2) * ' '}╯" for r in row2])
+            row2 = " * ".join(row2)
+            assert len(row1) == len(row2)
+            assert len(row2) == len(row3)
+            return row1, row2, row3
+        else:
+            raise Exception
+
+    @staticmethod
+    def _allclose(a: np.ndarray, b: np.ndarray, rtol: float, atol: float) -> bool:
+        return len(a) == len(b) and np.allclose(a=a, b=b, rtol=rtol, atol=atol);
 
 
-ZeroPole.ZERO = ZeroPole(0, [], [])
-ZeroPole.ONE = ZeroPole(1, [], [])
-ZeroPole.DIFF = ZeroPole(1, [1], [])
-ZeroPole.DELAY = ZeroPole(1, [], [], 1)
+ZeroPole.ZERO_ANALOG = ZeroPole(ZeroPoleType.ANALOG, 0, [], [])
+ZeroPole.ZERO_DIGITAL = ZeroPole(ZeroPoleType.DIGITAL, 0, [], [])
+
+ZeroPole.ONE_ANALOG = ZeroPole(ZeroPoleType.ANALOG, 1, [], [])
+ZeroPole.ONE_DIGITAL = ZeroPole(ZeroPoleType.DIGITAL, 1, [], [])
+
+ZeroPole.DIFF_ANALOG = ZeroPole(ZeroPoleType.ANALOG, 1, [1], [])
+ZeroPole.DIFF_DIGITAL = ZeroPole(ZeroPoleType.DIGITAL, 1, [1], [])
+
+ZeroPole.DELAY_DIGITAL = ZeroPole(ZeroPoleType.DIGITAL, 1, [], [], 1)
 
 
-def create_nlp(tau: float, order: int) -> ZeroPole:
-    nlp = ZeroPole.ZERO
+def create_nlp(type: ZeroPoleType, tau: float, order: int) -> ZeroPole:
+    nlp = ZeroPole.ZERO_ANALOG if type == ZeroPoleType.ANALOG else ZeroPole.ZERO_DIGITAL
     for i in range(order):
-        nlp += ZeroPole.lp1(1 / (tau/order)) ** (i + 1)
+        nlp += ZeroPole.lp1(type, 1 / (tau/order)) ** (i + 1)
 
     nlp /= order
     return nlp
